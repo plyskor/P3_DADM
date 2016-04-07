277528048a9149c2dee6714a15709d4305b6bbd9
Jose Antnonio García del Saz <plyskor@gmail.com>
2016-04-07 16:57:08 +0200

    about.xml

--- app/src/main/java/res/layout/about.java
+++ app/src/main/java/res/layout/about.java
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.jose.connect3.R;
 public class About extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
